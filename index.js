4f053fc3d8e310fe13aa1f9f10b4ce3a0ec42a41
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2021-10-07 23:25:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const hacktoberfest = require('hacktoberfest-issue-hunt');
 const PORT = process.env.PORT || 3001;
 const path = require('path');
-let cors = require("cors");
+//let cors = require("cors");
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+//app.use(cors());
  
 options = {
     token: process.env.MY_TOKEN,
