0cd4897cba52aac517202976b5ef9f1c4b266212
Krish Gupta <krishguptadev@outlook.com>
2021-10-25 04:12:07 +0000

    feat: issue status

--- src/Components/IssueCard.jsx
+++ src/Components/IssueCard.jsx
@@ -8,8 +8,10 @@ import Card from "react-bootstrap/Card";
 export default function IssueCard(props) {
     return (
       <Card>
-        <Card.Body> <Card.Link target="_blank"  href={ props.link}>{props.title }</Card.Link> </Card.Body>
-        
+        <Card.Body>
+          <Card.Link target="_blank"  href={ props.link}>{props.title }</Card.Link>
+          <Card.Text style={{margin: '0.25rem 0'}}> {props.status.charAt(0).toUpperCase() + props.status.slice(1)} </Card.Text>
+        </Card.Body>
       </Card>
     );
 }
