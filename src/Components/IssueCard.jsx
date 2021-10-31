c5d6f23d642abd718762c6b37f34461d64554c9e
Amritesh Anand <73696688+amritesh19@users.noreply.github.com>
2021-10-31 20:08:04 +0530

    Added Badge to show label of different colors

--- src/Components/IssueCard.jsx
+++ src/Components/IssueCard.jsx
@@ -1,15 +1,45 @@
 import React from "react"
 import Card from "react-bootstrap/Card";
+import Badge from 'react-bootstrap/Badge';
 
-// let cardStyle = {
-
-// }
+function labelBg(label) {
+  switch (label) {
+  case "good first issue":
+    return "success";
+  case "enhancement":
+    return "info";
+  case "hacktoberfest":
+    return "warning";
+  case "Hacktoberfest":
+    return "warning";
+  case "bug":
+    return "danger";
+  case "help wanted":
+    return "secondary";
+  case "hacktoberfest-accepted":
+    return "danger";
+  case "feature":
+    return "dark";
+  default:
+    return "primary";
+}
+}
 
 export default function IssueCard(props) {
     return (
       <Card>
-        <Card.Body> <Card.Link target="_blank"  href={ props.link}>{props.title }</Card.Link> </Card.Body>
-        
+        <Card.Body> 
+          <Card.Link target="_blank"  href={props.link}>{props.title }</Card.Link>
+          <div>
+          {props.labels.map((element,index)=>{
+             return(
+              <Badge key={index} pill bg={labelBg(element)}>
+                  {element}
+              </Badge>
+             );
+          })}
+          </div>
+        </Card.Body>
       </Card>
     );
 }
